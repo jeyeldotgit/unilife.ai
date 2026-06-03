42411f41d89f39a1cdf3f9cfa24e3262305351f2
jeyeldotgit <jeyel.git@gmail.com>
2026-06-03 13:33:02 +0800

    fix(backend): modified workspace files and tested packages import

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,15 +1,30 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-const app = new Hono()
+import type { User } from "@unilife-ai/types";
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const user1: User = {
+  id: "123e4567-e89b-12d3-a456-426614174000",
+  name: "John Doe",
+  display_name: "John Doe",
+  avatar_url: "https://example.com/avatar.jpg",
+  created_at: "april 20",
+  updated_at: "2023-01-01T00:00:00Z",
+};
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+const app = new Hono();
+
+app.get("/", (c) => {
+  const jsonifiedUser = JSON.stringify(user1);
+  return c.text(jsonifiedUser, 200, { "Content-Type": "application/json" });
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
