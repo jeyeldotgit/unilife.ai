03fee142da086a9bded0f4c2457a1ac2ae0806e8
jeyeldotgit <jeyel.git@gmail.com>
2026-06-13 17:58:27 +0800

    fix(fe-deployment): fix icon

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -17,9 +17,9 @@ export const metadata: Metadata = {
   description:
     "Your AI-powered university assistant. Get personalized help with course selection, study planning, and more. Simplify your university experience with UniLife.AI.",
   icons: {
-    icon: "/unilife-ai.svg",
-    shortcut: "/unilife-ai.svg",
-    apple: "/unilife-ai.svg",
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
   },
 };
 
